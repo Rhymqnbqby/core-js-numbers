22f47cfc9ee7ac9e5f42d591d5bbac4e88efe730
Rhymqnbqby <graffiti211@yandex.ru>
2024-11-17 00:11:17 +0300

    fix: fix one more time

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -50,7 +50,7 @@ function getCircleCircumference(radius) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAcerage(value1, value2) {
+function getAverage(value1, value2) {
   if (value1 === Number.MAX_VALUE && value2 === Number.MAX_VALUE) {
     return Number.MAX_VALUE;
   }
